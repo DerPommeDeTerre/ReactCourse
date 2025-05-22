3829303fe05281b6228973d91b4cf943153271cb
DerPommeDeTerre <ljmm9658@gmail.com>
2025-05-21 22:42:21 -0600

    Changed handleSubmit to addIngredient using form internal state

--- Course/S6/s6/src/components/Main.jsx
+++ Course/S6/s6/src/components/Main.jsx
@@ -2,30 +2,40 @@ import React from 'react'
 
 export const Main = () => {
 
-  const [ingredients, setIngredients] = React.useState(["Chicken", "Oregano", "Tomatoes"])
+  const [ingredients, setIngredients] = React.useState([])//Previous array ["Chicken", "Oregano", "Tomatoes"]
   const ingredientsElements = ingredients.map(function(elements){
     return <li key = {elements}>{elements}</li>
   })
+  //----------handleSubmit to add new ingredient
+  // function handleSubmit(event){
+  //   event.preventDefault();//Avoid refreshing
+  //   console.log("Ingredient submited")
 
-  function handleSubmit(event){
-    event.preventDefault();//Avoid refreshing
-    console.log("Ingredient submited")
+  //   const formData = new FormData(event.currentTarget)// Reads all fields in the Form
+  //   const newIngredient = formData.get("ingredient")//Retrieves value from the input "ingredient"
+  //   console.log(newIngredient)
 
-    const formData = new FormData(event.currentTarget)// Reads all fields in the Form
-    const newIngredient = formData.get("ingredient")//Retrieves value from the input "ingredient"
+  //   // ingredients.push(newIngredient);
+  //   // console.log(ingredients + "sdada");
+    
+  //   setIngredients(function(prevValue){
+  //     return [...prevValue, newIngredient]
+  //   })
+  // }
+//-----------Handling new ingredientes input without handleSubmit
+  function addIngredient(formData){
+    const newIngredient = formData.get("ingredient")
     console.log(newIngredient)
 
-    // ingredients.push(newIngredient);
-    // console.log(ingredients + "sdada");
-    
     setIngredients(function(prevValue){
-      return [...prevValue, newIngredient]
+      return[...prevValue, newIngredient]
     })
   }
 
   return (
    <main className='form-container'>
-    <form className='form' onSubmit={handleSubmit}>
+    <form action={addIngredient} className='form'>
+    {/* <form className='form' onSubmit={handleSubmit}> */}
         <input name = "ingredient" className="form-input" type="text" placeholder='e.g. oregano'/>
         <button className='form-btn'>+ Add ingredient</button>
     </form>
